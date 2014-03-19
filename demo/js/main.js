cfbeb5a4fae8281974db1f4bd834bd65f8ca0ebf
U-CORP\alovell <alovell@US-ALOVELL.corp.nuskin.net>
2014-03-19 10:29:53 -0600

    Included a Demo folder to show off the tool

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,5 +1,22 @@
-$(document).ready(
-	function(){
-		console.log($('#testArea').text().toNumber());
-	}	
-);
+$(document).on('click', '#toMoney>button', convertToMoney);
+$(document).on('click', '#toComma>button', convertToComma);
+$(document).on('click', '#toNumber>button', convertToNumber);
+
+function convertToMoney(){
+	$('#toMoney>p').text(
+		$('#toMoney>input').val().toMoney()
+	);
+}
+
+
+function convertToComma(){
+	$('#toComma>p').text(
+		$('#toComma>input').val().toComma()
+	);
+}
+
+function convertToNumber(){
+	$('#toNumber>p').text(
+		$('#toNumber>input').val().toNumber()
+	);
+}
